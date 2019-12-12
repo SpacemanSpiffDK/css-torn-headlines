e8bb6dfcb5d25677fa8df4e03e67ad04cd2a436a
Dan Høegh <dan.hoegh@gmail.com>
2019-12-12 19:11:46 +0100

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
 // JS by Dan Høegh
 // UCN MMD 2019
+
+const selectorTarget = '.torn';
+const elms = document.querySelectorAll(selectorTarget);
+
+for (let i = 0; i < elms.length; i++) {
+    let elm = elms[i];
+    let elmText = elm.textContent;
+    elm.textContent = '';
+    for (let j = 0; j < 3; j++) {
+        let newElm = document.createElement("span");
+        newElm.textContent = elmText;
+        if (j > 0){
+            newElm.classList.add('torn-elm');
+            newElm.classList.add('torn-' + j);
+        } else {
+            newElm.classList.add('torn-base');
+        }
+        elm.appendChild(newElm);
+    }
+}
